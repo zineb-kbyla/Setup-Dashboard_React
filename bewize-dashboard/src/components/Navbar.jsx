eec4eb6b9b013d813d7d7c29b2f63c0a3b8bb190
youssef88ab <youssefaboueljihade@gmail.com>
2025-06-25 16:51:18 +0100

    Enhance dashboard: update charts, metrics, layout, and navigation components for improved UI/UX and data visualization.

--- bewize-dashboard/src/components/Navbar.jsx
+++ bewize-dashboard/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar({ onToggleSidebar }) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <header className="w-full shadow px-4 py-3 flex justify-between items-center bg-white border-b border-gray-200 dark:bg-[#0f1c3e] dark:border-gray-700">
+    <header className="w-full shadow px-4 py-3 flex justify-between items-center  border-b  bg-[#0f1c3e] border-gray-700">
       <button
         onClick={onToggleSidebar}
         data-sidebar-toggle
